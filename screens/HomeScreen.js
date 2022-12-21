bb8530c7f6f6ac2b2215a3082f77128f8ebd84a0
khurelbt <khurelbt@oclc.org>
2022-12-21 17:46:18 -0500

    added google nav

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,11 +2,16 @@ import { View, Text, SafeAreaView, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import tw from 'twrnc';
 import NavOptions from '../component/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_API_KEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 
 const HomeScreen = () => {
+    const dispatch = useDispatch(); 
     return (
-        <SafeAreaView style= {tw`bg-white h-full`}>
+        <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
                 <Image
                     style={{
@@ -14,8 +19,51 @@ const HomeScreen = () => {
                         height: 100,
                         resizeMode: 'contain',
                     }}
-                    source={{ uri: "https://links.papareact.com/gzs" }} />
+                    source={{ uri: "https://links.papareact.com/gzs" }}
+                />
 
+                <GooglePlacesAutocomplete
+                    placeholder='Where From?'
+                    debounce={400}
+                    minLength={2} // minimum length of text to search
+                    autoFocus={false}
+                    returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+                    listViewDisplayed='auto'    // true/false/undefined
+                    fetchDetails={true}
+                    renderDescription={row => row.description} // custom description render
+
+                    onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                        dispatch(setOrigin({
+                            location: details.geometry.location, 
+                            description: data.description
+                        }))
+
+                        dispatch(setDestination(null))
+                        console.log(data, details);
+                    }}
+                    enablePoweredByContainer={false}
+
+                    query={{
+                        key:  GOOGLE_MAPS_API_KEY ,
+                        language: "en"
+                    }}
+
+                    styles={{
+                        container: {
+                            flex: 0
+                        },
+
+                        textInputContainer: {
+                            width: '100%'
+                        },
+                        description: {
+                            fontWeight: 'bold'
+                        },
+                        predefinedPlacesDescription: {
+                            color: '#1faadb'
+                        }
+                    }}
+                />
                 <NavOptions />
             </View>
         </SafeAreaView>
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+import MapView, {Marker} from 'react-native-maps';
+import Map from '../component/Map';
 
 const MapScreen = () => {
   return (
     <View>
-      <Text>MapScreen</Text>
+      <View style={tw`h-1/2`} >
+        <Map />
+      </View>
+      <View style={tw`h-1/2`} ></View>
+
     </View>
+
   )
 }
 
