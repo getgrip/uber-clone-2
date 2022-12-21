63802b6c7abb2d367ca917cdae6ef5ba420e8a22
khurelbt <khurelbt@oclc.org>
2022-12-20 22:22:32 -0500

    added stack-navigation

--- App.js
+++ App.js
@@ -4,15 +4,39 @@ import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
+import 'react-native-gesture-handler';
 
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <Provider store={store}>
-        <HomeScreen />
-      </Provider>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
 
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,18 @@
 import { View, Text, SafeAreaView, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import tw from 'twrnc';
-import NavOptions from './component/NavOptions';
+import NavOptions from '../component/NavOptions';
 
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView>
-            <View style= {tw`p-5`}>
+        <SafeAreaView style= {tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
                 <Image
-                    style = {{
-                        width:100, height:100, resizeMode: 'contain',
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: 'contain',
                     }}
                     source={{ uri: "https://links.papareact.com/gzs" }} />
 
